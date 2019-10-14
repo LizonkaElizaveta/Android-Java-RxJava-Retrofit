512630ac6fb9ed2a7edde1b34c1d426cf2747ece
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-10-14 23:47:37 +0300

    create CardService and JsonCardApi interface with method to get data

--- app/src/main/java/stanevich/elizaveta/exchangerates/network/service/CardApiService.java
+++ app/src/main/java/stanevich/elizaveta/exchangerates/network/service/CardApiService.java
@@ -1,4 +1,39 @@
 package stanevich.elizaveta.exchangerates.network.service;
 
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import stanevich.elizaveta.exchangerates.network.data.card.CardProperty;
+
 public class CardApiService {
+
+    private static CardApiService mInstance;
+    private static final String BASE_URL = "https://peterpartner.net/tasktest";
+    private Retrofit mRetrofit;
+
+    private CardApiService() {
+        mRetrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static CardApiService getInstance() {
+        if (mInstance == null) {
+            mInstance = new CardApiService();
+        }
+        return mInstance;
+    }
+
+
+    public interface JSONPCardApi {
+        @GET("/data.php")
+        Call<CardProperty> getCardProperty();
+    }
+
+    public JSONPCardApi getJSONCardApi() {
+        return mRetrofit.create(JSONPCardApi.class);
+    }
+
 }
