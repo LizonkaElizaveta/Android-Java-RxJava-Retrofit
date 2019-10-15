f8a3d458c1f3dbcc41cd4481fe5ae9f80f88ce66
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-10-15 17:10:14 +0300

    create api service for exchange rates

--- app/src/main/java/stanevich/elizaveta/exchangerates/network/service/ExchangeRatesApiService.java
+++ app/src/main/java/stanevich/elizaveta/exchangerates/network/service/ExchangeRatesApiService.java
@@ -1,4 +1,38 @@
 package stanevich.elizaveta.exchangerates.network.service;
 
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import stanevich.elizaveta.exchangerates.network.data.rates.ExchangeRatesProperty;
+
 public class ExchangeRatesApiService {
+    private static ExchangeRatesApiService mInstance;
+    private static final String BASE_URL = "https://www.cbr-xml-daily.ru";
+    private Retrofit mRetrofit;
+
+    private ExchangeRatesApiService() {
+        mRetrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static ExchangeRatesApiService getInstance() {
+        if (mInstance == null) {
+            mInstance = new ExchangeRatesApiService();
+        }
+        return mInstance;
+    }
+
+
+    public interface JSONPRatesApi {
+        @GET("/daily_json.js")
+        Call<ExchangeRatesProperty> getExchangeRatesProperty();
+    }
+
+    public JSONPRatesApi getJSONRatesApi() {
+        return mRetrofit.create(JSONPRatesApi.class);
+    }
+
 }
